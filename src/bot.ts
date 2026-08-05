4c0e05a3363bd6cc8489bee77971450b7236d0a6
Eric <ericjohnariate@gmail.com>
2026-08-05 15:04:21 +0800

    initial commit

--- src/bot.ts
+++ src/bot.ts
@@ -3,7 +3,7 @@ import "dotenv/config";
 import { Client, Collection, GatewayIntentBits, Partials } from "discord.js";
 import { config } from "./config.js";
 
-import { registerEvents } from "./events/indext.js";
+import { registerEvents } from "./events/index.js";
 
 declare module "discord.js" {
   interface Client {
