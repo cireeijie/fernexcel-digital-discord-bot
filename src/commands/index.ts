8482e06667decf73ebfacbb5716c108358122243
Eric <ericjohnariate@gmail.com>
2026-08-05 15:30:13 +0800

    initial commit

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,7 +1,13 @@
 import { pingCommand } from "./ping.js";
 import { setupCommand } from "./setup.js";
 
-export const commands = {
+import type { ChatInputCommandInteraction } from "discord.js";
+
+export type CommandHandler = (
+  interaction: ChatInputCommandInteraction,
+) => Promise<unknown>;
+
+export const commands: Record<string, CommandHandler> = {
   ping: pingCommand,
   setup: setupCommand,
 };
